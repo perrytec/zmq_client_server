eccd57e0477d6a9b234387a08f04b408892b253e
Bill <xiaobill8@gmail.com>
2024-03-11 12:18:36 +0800

    Adds req-rep pattern

--- zmq_client.py
+++ zmq_client.py
@@ -15,26 +15,36 @@ class ZmqClient():
             self.req_socket = self.zmq_context.socket(zmq.REQ)
             self.req_socket.connect(req_ip)
 
+    # Pub sub
     def subscribe(self, topic):
         print(f"Subscribing: {topic}")
         self.sub_socket.subscribe(topic)
 
-    def receive(self):
+    def receive_sub(self):
         return self.sub_socket.recv_string()
 
+    # Push pull
     def push(self, topic, data):
         print(f"Sending: {topic} {data}")
         self.push_socket.send_string(f"{topic} {data}")
 
-    def close(self):
-        self.pub_socket.close()
-        self.zmq_context.term()
+    # Request reply
+    def request(self, data):
+        self.req_socket.send_string(data)
+        return self.req_socket.recv_string()
 
-    # Add request reply for Python exchange_connectors
+    def close(self):
+        if hasattr(self, 'sub_socket'):
+            self.sub_socket.close()
+        if hasattr(self, 'push_socket'):
+            self.push_socket.close()
+        if hasattr(self, 'req_socket'):
+            self.req_socket.close()
+        self.zmq_context.term()
 
 
 if __name__ == "__main__":
-    server = ZmqClient("tcp://127.0.0.1:5555", "tcp://127.0.0.1:5556")
+    server = ZmqClient("tcp://127.0.0.1:5555", "tcp://127.0.0.1:5556", "tcp://127.0.0.1:5557")
     end_time = time.time() + 900  # 15 minutes
 
     server.subscribe("589141846_tob")
@@ -42,5 +52,5 @@ if __name__ == "__main__":
         server.push("589141846", "top_of_book")
         time.sleep(30)
 
-    server.pub_socket.close()
+    server.close()
     server.zmq_context.term()
--- zmq_server.py
+++ zmq_server.py
@@ -5,6 +5,7 @@ import time
 class ZmqServer():
     def __init__(self, pub_ip: str = None, pull_ip: str = None, rep_ip: str = None):
         self.zmq_context = zmq.Context()
+        self.poller = zmq.Poller()
         if pub_ip:
             self.pub_socket = self.zmq_context.socket(zmq.PUB)
             self.pub_socket.bind(pub_ip)
@@ -15,34 +16,51 @@ class ZmqServer():
             self.rep_socket = self.zmq_context.socket(zmq.REP)
             self.rep_socket.bind(rep_ip)
 
+    # Pub sub
     def publish(self, topic, data):
         print(f"Sending: {topic} {data}")
         self.pub_socket.send_string(f"{topic} {data}")
 
-    def receive(self):
+    def receive_pub(self):
         return self.pub_socket.recv_string()
 
+    # Push pull
     def poll_pull_socket(self):
         return self.pull_socket.poll(1)
 
-    def pull(self):
+    def receive_pull(self):
         return self.pull_socket.recv_string()
 
-    def close(self):
-        self.pub_socket.close()
-        self.zmq_context.term()
+    # Request reply
+    # Req-Rep pattern is synchronous - client blocks until server replies
+    def poll_rep_socket(self):
+        return self.rep_socket.poll(1)
+    
+    def reply(self, data):
+        self.rep_socket.send_string(data)
 
-    # Add request reply for Python exchange_connectors
+    def receive_req(self):
+        return self.rep_socket.recv_string()
+
+    def close(self):
+        if hasattr(self, 'pub_socket'):
+            self.pub_socket.close()
+        if hasattr(self, 'pull_socket'):
+            self.pull_socket.close()
+        if hasattr(self, 'rep_socket'):
+            self.rep_socket.close()
+        self.zmq_context.term()
 
 
 if __name__ == "__main__":
-    server = ZmqServer("tcp://127.0.0.1:5555", "tcp://127.0.0.1:5556")
+    server = ZmqServer("tcp://127.0.0.1:5555",
+                       "tcp://127.0.0.1:5556", "tcp://127.0.0.1:5557")
     end_time = time.time() + 900  # 15 minutes
 
     while time.time() < end_time:
         if server.poll_pull_socket() > 0:
             print(server.pull())
         time.sleep(10)
-
-    server.pub_socket.close()
+    
+    server.close()
     server.zmq_context.term()
