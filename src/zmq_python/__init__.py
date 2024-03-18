7b50a0f8b54c6895d8b46dab4bb096ac6a2788d4
Bill <xiaobill8@gmail.com>
2024-03-18 17:02:14 +0800

    Fixes import path

--- src/zmq_python/__init__.py
+++ src/zmq_python/__init__.py
@@ -1,2 +1,2 @@
-from zmq_python.src.zmq_python.zmq_client import ZmqClient
-from zmq_python.src.zmq_python.zmq_server import ZmqServer
+from zmq_python.zmq_client import ZmqClient
+from zmq_python.zmq_server import ZmqServer
